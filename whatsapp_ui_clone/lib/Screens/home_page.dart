3064b4429772eb925ccea3b8ee4614f22c1ee05a
Sajal Chandaiya <sajal@rashailinfotech.co.in>
2026-02-08 13:05:32 +0530

    added local DB

--- whatsapp_ui_clone/lib/Screens/home_page.dart
+++ whatsapp_ui_clone/lib/Screens/home_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:whatsapp_ui_clone/Constants/custom_colors.dart';
 import 'package:whatsapp_ui_clone/Providers/api_provider.dart';
 
+import '../Helpers/dbhelper.dart';
 import '../Widgets/styled_text.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,10 +14,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-    context.read<ApiProvider>().getApiData(context: context);
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   context.read<ApiProvider>().getApiData(context: context);
+  // }
+
+  final dbhelper = Databasehelper.instance;
+
+  void insertData() async {
+    Map<String, dynamic> row = {
+      Databasehelper.columnName: "Sajal",
+      Databasehelper.columnAge: 21,
+    };
+    final id = await dbhelper.insert(row);
+    print(id);
+  }
+
+  void queryAllData() async {
+    var allRows = await dbhelper.queryAll();
+    // allRows.forEach((element) {
+    print(allRows.toString());
+    // });
+  }
+
+  void querySpecificData() async {
+    var allRows = await dbhelper.querySpecific(3);
+    allRows.forEach((element) {
+      print(allRows.toString());
+    });
+  }
+
+  void deleteData() async {
+    var reply = await dbhelper.deleteData(3);
+    print(reply);
+  }
+
+  void updateData() async {
+    var reply = await dbhelper.update(1);
+    print(reply);
   }
 
   @override
@@ -38,10 +74,40 @@ class _HomePageState extends State<HomePage> {
           children: [
             ElevatedButton(
               onPressed: () {
-                context.read<ApiProvider>().loginWithGoogle(context: context);
+                insertData();
               },
-              child: StyledText(text: "Login with google"),
+              child: Text("Insert"),
             ),
+            ElevatedButton(
+              onPressed: () {
+                queryAllData();
+              },
+              child: Text("Query"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                querySpecificData();
+              },
+              child: Text("Query Specific"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                updateData();
+              },
+              child: Text("Update"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                deleteData();
+              },
+              child: Text("Delete"),
+            ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     context.read<ApiProvider>().loginWithGoogle(context: context);
+            //   },
+            //   child: StyledText(text: "Login with google"),
+            // ),
             // ...context.watch<ApiProvider>().apiData.map(
             //   (e) => Container(
             //     decoration: BoxDecoration(border: Border.all()),
